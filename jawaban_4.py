789ce46cffdabd6c5c29c932ab0777a4caab1cf5
akhfaiqm <faiqmursyidi732@gmail.com>
2024-11-13 12:16:45 +0700

    Add files via upload

--- jawaban_4.py
+++ jawaban_4.py
@@ -1,18 +1,17 @@
-inputanBb = 0
-inputanTb = 0
 
 for i in range():
-    inputanBb = int(input{"Masukan Berat Badan (kg):"})
-    inputanTb = int(input{"Masukan Tinggi Badan (cm):"})
+    inputanBb = int(input("Masukan Berat Badan (kg):"))
+    inputanTb = int(input("Masukan Tinggi Badan (cm):"))
 
-   hasiltimbang = inputanBb // inputanTb
+   hasiltimbang = inputanBb /inputanTb**2
 
-      if hasiltimbang < 18.5:
-         tipe = hasiltimbang <18.5
-         print('Berat badan kurang : ')
+    if hasiltimbang < 18.5:
+        print('Berat badan kurang : ')
 
-     elif 18.5 <= hasiltimbang < 24.9:
-         tipe = hasiltimbang <24.9
-          print('berat badan normal :')
-     elif 25 <= hasiltimbang <29.9 :
-   print (' berat badan kelebihan :')
+    elif 18.5 <= hasiltimbang < 24.9:
+            
+        print('berat badan normal :')
+
+    elif 25 <= hasiltimbang <29.9 :
+        
+        print (' berat badan kelebihan :')
